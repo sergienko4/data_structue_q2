92df9def5f0026d9c2ccdaf168aea336b7b44d52
Eugene Sergienko <Sergienko4@gmail.com>
2018-08-18 13:30:18 +0300

    UI essue fixed

--- data_structue_q2/data_structue_q2/Program.c
+++ data_structue_q2/data_structue_q2/Program.c
@@ -7,8 +7,16 @@ int isNumOrOperator(char c);
 char getChar();
 int preoretyId(char c);
 int getPreorities(char a, char b);
+void calculate(stack* s1, stack* s2, char c);
 char todo(stack* s1, stack* s2);
+
+void EX1();
 void main() {
+	
+	EX1();
+}
+
+void EX1() {
 	stack* s1;
 	stack* s2;
 	int i, length = 0, result;
@@ -33,7 +41,7 @@ void main() {
 			return;
 		}
 
-		
+
 		// is char
 		if (result == 1) {
 			push(str[i], s1);
@@ -47,66 +55,64 @@ void main() {
 				}
 				else {
 					stack_top(s2, &temp);
-					result= getPreorities(temp,str[i]); 
-					if(result ==0)
+					result = getPreorities(temp, str[i]);
+					if (result == 0)
 					{
 						push(str[i], s2);
 					}
 					else
 					{
 						push(todo(s1, s2), s1);
-
-						while (stack_is_empty(s2) == 0) {
-							stack_top(s2, &temp);
-							result = getPreorities(temp, str[i]);
-							if (result) {
-								push(todo(s1, s2),s1);
-							}
-							else {
-								break;
-							}
-						}
+						// check if can calculate in stack
+						calculate(s1, s2, str[i]);
 						push(str[i], s2);
 					}
 				}
-
-
-				//push(str[i], s2);
 			}
 		}
 	}
-	
+
+	// check if can calculate in stack last options
 	if (stack_is_empty(s1) == 0) {
-		while (stack_is_empty(s2) == 0) {
-			stack_top(s2, &temp);
-			result = getPreorities(temp, str[i]);
-			if (result) {
-				push(todo(s1, s2), s1);
-			}
-			else {
-				break;
-			}
-		}
-		pop(s1,&temp);
+
+		calculate(s1, s2, str[i]);
+		pop(s1, &temp);
 		free(s1);
 		free(s2);
 	}
 
 }
+// print operetion order
+void calculate(stack* s1, stack* s2, char c) {
+	int result;
+	char temp;
+	while (stack_is_empty(s2) == 0) {
+		stack_top(s2, &temp);
+		result = getPreorities(temp,c);
+		if (result) {
+			push(todo(s1, s2), s1);
+		}
+		else {
+			break;
+		}
+	}
+}
 
-char  todo(stack* s1, stack* s2) {
-	char temp,temp1,temp2,temp3;
+// print the output
+char todo(stack* s1, stack* s2) {
+	char temp, temp1, temp2, temp3;
 
 	pop(s2, &temp2);
 
 	pop(s1, &temp);
 	pop(s1, &temp1);
-	
+
 	temp3 = getChar();
 	printf("%C%C%C=%C\n", temp1, temp2, temp, temp3);
 	return temp3;
 }
 
+// check who is bigger
 int getPreorities(char a, char b) {
 
 	if (preoretyId(a) >= preoretyId(b))
@@ -114,7 +120,7 @@ int getPreorities(char a, char b) {
 	else
 		return 0;
 }
-
+// set preority
 int preoretyId(char c) {
 
 	switch (c)
@@ -131,6 +137,7 @@ int preoretyId(char c) {
 		return 3;
 	case '^':
 		return 4;
+	default: return -1;
 	}
 }
 
@@ -138,13 +145,13 @@ char getChar() {
 	char c;
 	c = ASCIICode;
 	ASCIICode--;
-	return c;
+	return (char)c;
 }
 
 // 1 char  , 0 operator , -1 not a char
 int isNumOrOperator(char c) {
 
-	if ((c >= 'A'&& c <= 'Z') || (c>='0' && c<='9')) {
+	if ((c >= 'A'&& c <= 'Z') || (c >= '0' && c <= '9')) {
 		return 1;
 	}
 	else {
